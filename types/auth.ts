820858a9d80feca487937b56e855b71e16c4ae01
Kendrick <kendrickoppong94@gmail.com>
2026-02-12 18:17:19 +0000

    add auth types

---
+++ types/auth.ts
@@ -0,0 +1,17 @@
+export interface AuthUser {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+  avatarUrl: string | null;
+  isVerified: boolean;
+  isOnboarded: boolean;
+  shouldResetPassword: boolean;
+}
+
+export interface AuthState {
+  user: AuthUser | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
