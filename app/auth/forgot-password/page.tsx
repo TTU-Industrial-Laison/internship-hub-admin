713cc83e532f978174da1a5be2787547e4e5afc6
Kendrick <kendrickoppong94@gmail.com>
2026-02-15 20:03:44 +0000

    add metadata

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,4 +1,10 @@
 import { ForgotPasswordForm } from "@/components/auth/forgot-password-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Forgot Password | InternshipHub",
+  description: "Forgot your password? Enter your email to reset it",
+};
 
 export default function ForgotPasswordPage() {
   return <ForgotPasswordForm />;
