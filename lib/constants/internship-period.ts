fc09e59f776bc7edb2e382a6611cb2864f25cde7
Kendrick <kendrickoppong94@gmail.com>
2026-02-16 22:02:52 +0000

    add constants export

--- lib/constants/internship-period.ts
+++ lib/constants/internship-period.ts
@@ -1,4 +1,16 @@
+import { MultiSelectOption } from "@/types/api/internship-period";
+
 export const INTERNSHIP_STATUS_OPTIONS = [
   { value: "ONGOING", label: "Ongoing" },
   { value: "COMPLETED", label: "Completed" },
+];
+
+export const eligibleCategories = ["MTECH", "BTECH", "BTECH_TOPUP", "HND", "DIPLOMA"];
+
+export const CATEGORY_OPTIONS: MultiSelectOption[] = [
+  { label: "MTECH", value: "MTECH" },
+  { label: "BTECH", value: "BTECH" },
+  { label: "BTECH TOPUP", value: "BTECH_TOPUP" },
+  { label: "HND", value: "HND" },
+  { label: "DIPLOMA", value: "DIPLOMA" },
 ];
