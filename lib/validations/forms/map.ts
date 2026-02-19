1c730fccdf4288632e0d241cdfd20bbb1b500bba
Kendrick <kendrickoppong94@gmail.com>
2026-02-19 21:56:17 +0000

    add zone schema

---
+++ lib/validations/forms/map.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const zoneSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+  color: z.string().min(1, "Color is required"),
+  transparency: z.number().min(0).max(1),
+  borderWidth: z.number().min(0).max(10),
+});
+
+export type ZoneFormData = z.infer<typeof zoneSchema>;
